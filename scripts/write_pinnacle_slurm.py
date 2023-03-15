d339ddad2f3e0b902cd285dba7ef3ea9c9af47dc
Fa1sal-Alqhtani <faisal.v8@gmail.com>
2023-03-15 08:54:49 -0500

    Create write_pinnacle_slurm.py

---
+++ scripts/write_pinnacle_slurm.py
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#SBATCH --job-name=python_script
+#SBATCH --partition comp72
+#SBATCH --nodes=1
+#SBATCH --tasks-per-node=1
+#SBATCH --time=72:00:00
+#SBATCH -o unknown.out
+#SBATCH -e unknown.err
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=fhalqhta@uark.edu
+
+cd $SLURM_SUBMIT_DIR
