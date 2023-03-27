de5eab9bd52df19aace04f36ea7d788df467d6b4
Fa1sal-Alqhtani <faisal.v8@gmail.com>
2023-03-27 09:03:30 -0500

    Update write_pinnacle_slurm.py

--- scripts/write_pinnacle_slurm.py
+++ scripts/write_pinnacle_slurm.py
@@ -1,14 +1,28 @@
-#!/bin/bash
+#! /usr/bin/env python3
 
-#SBATCH --job-name=python_script
-#SBATCH --partition comp72
-#SBATCH --nodes=1
-#SBATCH --tasks-per-node=1
-#SBATCH --time=72:00:00
-#SBATCH -o unknown.out
-#SBATCH -e unknown.err
-#SBATCH --mail-type=ALL
-#SBATCH --mail-user=fhalqhta@uark.edu
+# set some variables
+job_name = ''<job_name>'
+queue = 'comp01'
+walltime = 1
+num_nodes = 1
+num_processors = 24
+
+# print bash header
+print('#!/bin/bash')
+
+print()
+
+# print SBATCH commands
+print('#SBATCH --job-name= ' + job_name)
+print('#SBATCH --partition',  queue)
+print('#SBATCH --nodes=' + str(num_nodes))
+print('#SBATCH --qos comp')
+print('#SBATCH --tasks-per-node=' + )
+print('#SBATCH --time=72:00:00')
+print('#SBATCH -o unknown.out')
+print('#SBATCH -e unknown.err')
+print('#SBATCH --mail-type=ALL')
+print('#SBATCH --mail-user=fhalqhta@uark.edu')
 
 cd $SLURM_SUBMIT_DIR
 
