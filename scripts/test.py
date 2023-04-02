f364348f53566efeedb703d41ede3ccfbeb674fc
Fa1sal-Alqhtani <faisal.v8@gmail.com>
2023-04-02 18:38:53 -0500

    updating the SLURM file and did some testing
    updating the slurm file as described in Assignment 5.

--- scripts/test.py
+++ scripts/test.py
@@ -1,37 +1,6 @@
-#! /usr/bin/env python3
+#!/usr/bin/env python3
 
-# set some variables
-job_name = 'MlrA'
-queue = 'comp01'
-walltime = 1
-num_nodes = 1
-num_processors = 24
-
-# print bash header
-print('#!/bin/bash')
-
-print()
-
-# print SBATCH commands
-print('#SBATCH --job-name=' + job_name)
-print('#SBATCH --partition', queue)
-print('#SBATCH --nodes=' + str(num_nodes))
-print('#SBATCH --qos comp')
-print('#SBATCH --tasks-per-node=' + str(num_processors))
-print('#SBATCH --time=' + str(walltime) + ':00:00')
-print('#SBATCH -o aja_%j.out')
-print('#SBATCH -e aja_%j.err')
-print('#SBATCH --mail-type=ALL')
-print('#SBATCH --mail-user=fhalqhta@uark.edu')
-
-print()
-
-# purge all the modules
-print('module purge')
-
-from _typeshed import ExcInfo
 import argparse
-from pickle import EXT1
 
 # create an ArgumentParser object
 parser = argparse.ArgumentParser(description='Submit a job to Pinnacle cluster using SLURM')
@@ -47,9 +16,6 @@ parser.add_argument('--walltime', type=int, default=1, help='Walltime in hours')
 
 # parse the command-line arguments
 args = parser.parse_args()
-except SystemExit as e:
-print("Error parsing command-line arguments:", EXT1)
-sys.exit(1)
 
 # assign the parsed arguments to variables
 job_name = args.job_name
@@ -58,10 +24,32 @@ num_nodes = args.nodes
 num_processors = args.procs
 walltime = args.walltime
 
+# print bash header
+print('#!/bin/bash')
+
+print()
+
+# print SBATCH commands
+print('#SBATCH --job-name=' + job_name)
+print('#SBATCH --partition', queue)
+print('#SBATCH --nodes=' + str(num_nodes))
+print('#SBATCH --tasks-per-node=' + str(num_processors))
+print('#SBATCH --time=' + str(walltime) + ':00:00')
+print('#SBATCH -o ' + job_name + '_%j.out')
+print('#SBATCH -e ' + job_name + '_%j.err')
+print('#SBATCH --mail-type=ALL')
+print('#SBATCH --mail-user=aja@uark.edu')
+
+print()
+
+# purge all the modules
+print('module purge')
+
+print()
+
 # cd into the submit directory
 print('cd $SLURM_SUBMIT_DIR')
 
 print()
 
 print('# job command here')
-
