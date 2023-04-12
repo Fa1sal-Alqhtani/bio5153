451371729631fce69b7be0f97a3a80716a5d40af
Fa1sal-Alqhtani <faisal.v8@gmail.com>
2023-04-12 09:42:15 -0500

    Update parseGFF.py
    I added csv

--- scripts/parseGFF.py
+++ scripts/parseGFF.py
@@ -2,6 +2,7 @@
 
 # import modules
 import argparse
+import csv
 
 #create an ArgumentParser object
 parser = argparse.ArgumentParser(description="This script parses a GFF file")
@@ -17,28 +18,32 @@ args = parser.parse_args()
 # open the GFF file
 with open(args.gff) as gff_file:
 
+    # create a csv reader object
+    reader = csv.reader(gff_file, delimiter='\t')
+
     # loop over all the lines in the file
-    for line in gff_file:
+    for line in reader:
+
         # skip blank lines
-        if not line.strip():
+        if not line:
             continue
+
         # else it's not a blank line
         else:
-            # remove the newline character from each line
             line = line.rstrip()
 
             # separate each line into individual columns
             columns = line.split("\t")
 
             # give variable names to the columns
-            organism = columns[0]
-            source   = columns[1]
+            organism     = columns[0]
+            source       = columns[1]
             feature_type = columns[2]
-            start    = columns[3]
-            end      = columns[4]
-            length   = columns[5]
-            strand   = columns[6]
-            Attributes = columns[8]
+            start        = columns[3]
+            end          = columns[4]
+            length       = columns[5]
+            strand       = columns[6]
+            Attributes   = columns[8]
 
             # add the length to column 5 
             columns[5] = str(int(end) - int(start) + 1)
