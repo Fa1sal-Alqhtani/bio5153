0c46de39ad7be1a7443cc47290547890b1c9a193
Fa1sal-Alqhtani <faisal.v8@gmail.com>
2023-04-26 09:19:28 -0500

    Update calc-fibonacci.py
    updating the fibonacci.

--- scripts/calc-fibonacci.py
+++ scripts/calc-fibonacci.py
@@ -3,37 +3,55 @@
 # import modules
 import argparse
 
-#create an ArgumentParser object
-parser = argparse.ArgumentParser(description="This script returns the Fibonacci number at a specified locationin the sequence")
 
-# add positional (required) arguments
-parser.add_argument("num", help="The Fibonacci number you wish to calculate", type=int)
+# function to accept command-line arguments
+def get_args():
+        
+    #create an ArgumentParser object
+    parser = argparse.ArgumentParser(description="This script returns the Fibonacci number at a specified locationin the sequence")
 
-# add optional arguments
-# the default for 'store_true' is . . . "False"
-parser.add_argument("-v", "--verbose", help="Print verbose output", action= 'store_true')
+    # add positional (required) arguments
+    parser.add_argument("num", help="The Fibonacci number you wish to calculate", type=int)
 
-# parse the actual arguments
-# access argument values via 'args' variable
-args = parser.parse_args()
-print (args)
+    # add optional arguments
+    # the default for 'store_true' is . . . "False"
+    parser.add_argument("-v", "--verbose", help="Print verbose output", action= 'store_true')
 
-# calculate the Fibonacci number/sequence
-a,b = 0,1
+    # parse the actual arguments
+    # access argument values via 'args' variable
+    args = parser.parse_args()
+    
+    return args
 
-for i in range(args.num):
-    a,b = b,a+b
+# function to calculate the Fibonacci number/sequence
+def fib(n):
+    a,b = 0,1
 
-    fib_number = a 
+    for i in range(n):
+        a,b = b,a+b
 
+    return a
 
-# print the answer
-if args.verbose:
-    print('The Fibonacci number for', args.num, 'is:', fib_number)
-else:
-    print(fib_number)
+# define the 'main' function
+def main():
 
+    fib_number = fib(x.num)
+
+    # print the answer
+    if x.verbose:
+        print('The Fibonacci number for', x.num, 'is:', fib_number)
+    else:
+        print(fib_number)
+
+# get the command-line arguments
+
+x = get_args()
 # print the answer
 #print('The Fibonacci number for', args.num, 'is:' fib_number)
 
 
+
+# set the environment for this script
+# is this main, or is this a module called by another script?
+if __name__== '__main__':
+    main()
